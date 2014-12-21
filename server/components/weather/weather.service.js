06da9abf4ceb0a5c9bc5275c2b342a0a06b8fc36
TimGraf <tim-graf@comcast.net>
2014-12-21 11:35:47 -0800

    clean up img urls

--- server/components/weather/weather.service.js
+++ server/components/weather/weather.service.js
@@ -14,7 +14,7 @@ function WeatherService() {
       method: 'GET',
       url: 'http://api.wunderground.com/api/' + wuKey + '/forecast10day/q/CA/San_Francisco.json'
     }, function(err, wuRes, body) {
-      deferred.resolve(JSON.parse(body));
+      deferred.resolve(_fixForecastImgUrls(JSON.parse(body)));
     });
 
     return deferred.promise;
@@ -29,10 +29,24 @@ function WeatherService() {
       method: 'GET',
       url: 'http://api.wunderground.com/api/' + wuKey + '/conditions/q/CA/San_Francisco.json'
     }, function(err, wuRes, body) {
-      deferred.resolve(JSON.parse(body));
+      deferred.resolve(_fixConditionsImgUrl(JSON.parse(body)));
     });
 
     return deferred.promise;
+  }
+
+  function _fixConditionsImgUrl(conditionsJson) {
+    conditionsJson.current_observation.icon_url = conditionsJson.current_observation.icon_url.replace('/k/', '/v1/').replace('gif', 'svg');
+
+    return conditionsJson;
+  }
+
+  function _fixForecastImgUrls(forecastJson) {
+    forecastJson.forecast.txt_forecast.forecastday.forEach(function(forecastday, index) {
+      forecastJson.forecast.txt_forecast.forecastday[index].icon_url = forecastday.icon_url.replace('/k/', '/v1/').replace('gif', 'svg');
+    });
+
+    return forecastJson;
   }
 
   return {
